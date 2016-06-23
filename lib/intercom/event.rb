2a923285b96985b6811cbee21f0864d5be8a5a93
Anthony Pessy <anthony.pessy@gmail.com>
2016-06-23 10:35:42 +0200

    add support for find_all for Event model

--- lib/intercom/event.rb
+++ lib/intercom/event.rb
@@ -1,8 +1,10 @@
 require 'intercom/api_operations/save'
+require 'intercom/api_operations/find_all'
 require 'intercom/traits/api_resource'
 
 module Intercom
   class Event
+    include ApiOperations::FindAll
     include ApiOperations::Save
     include Traits::ApiResource
   end
